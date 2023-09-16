38a9c376cd393f601f62c6deb5e6759fbdec8b37
tanvirfsd <tahasanpro@gmail.com>
2023-09-16 15:53:05 +0600

    added a static enrolled component with enrolled.jsx

--- src/components/Enrolled.jsx
+++ src/components/Enrolled.jsx
@@ -1,7 +1,15 @@
 const Enrolled = () => {
   return (
-    <div className="md:w-1/3">
-      <h1>Enrolled</h1>
+    <div className="md:w-1/3 p-2 my-3">
+      <h2 className="text-[#2F80ED] text-lg font-semibold p-2">Credit Hour Remaining 7 hr</h2>
+      <hr />
+      <div className="p-2">
+        <h2 className="text-xl font-bold">Course Name</h2>
+      </div>
+      <hr />
+      <h3 className="font-medium p-2">Total Credit Hour : 13</h3>
+      <hr />
+      <h3 className="font-medium p-2">Total Price : 48000 USD</h3>
     </div>
   );
 };
